965ba84c04cf7863a1a3d21b02b27fe1a21bcc20
VaperMonkeyM <cainlopezburgos@gmail.com>
2025-12-03 02:09:24 +0100

    database

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROYECTO.firebaseapp.com",
-  projectId: "TU_PROYECTO",
-  storageBucket: "TU_PROYECTO.appspot.com",
-  messagingSenderId: "XXXXXXXX",
-  appId: "XXXXXXXX"
+  apiKey: "AIzaSyBSbd4J5xU0PSrXWJQIVxpFITOZF-m7HbQ",
+  authDomain: "pagina-web-vape.firebaseapp.com",
+  projectId: "pagina-web-vape",
+  storageBucket: "Tpagina-web-vape.firebasestorage.app",
+  messagingSenderId: "55550904480",
+  appId: "1:55550904480:web:d6568dcc68b8a403bf21c5"
 };
